5de1c9fcf968359a1eba874a59b60bfdc1b68c14
duy039 <hoperayno039@gmail.com>
2021-06-23 00:48:52 +0700

    đã ổn nav với footer

--- js/main.js
+++ js/main.js
@@ -1,14 +1,125 @@
 
-    if(localStorage.counter)
+    if(localStorage.view)
     {
-        localStorage.counter++;
+        localStorage.view++;
     }
     else{
-         //set giá trị cho  key counter
-        
-        localStorage.counter = 1;
+         //set giá trị 
+        localStorage.view = 1;
+      
     }
 
+    // lấy giá  trị
+    $(".view-web").html(localStorage.view)
+      
+    
 
-    // lấy giá  trị từ key counter
-    $(".view-web")[0].html(localStorage.counter);
+//////////////////////////////////////////   Đồng Hồ    ///////////////////////////////////////////////////
+        var festivalTime = new Date(2021, 05, 28).getTime() + (1000*3600*6); // time next festival
+        var days, hours, minutes, seconds; // variables for time units
+        
+        getCountdown();
+        setInterval(function () { getCountdown(); }, 1000);
+        function getCountdown(){
+        // find the amount of "seconds" between now and target
+        var current_date = new Date().getTime();
+        var seconds_left = (festivalTime - current_date) / 1000;
+
+        days = pad( parseInt(seconds_left / 86400) );
+        seconds_left = seconds_left % 86400;
+            
+        hours = pad( parseInt(seconds_left / 3600) );
+        seconds_left = seconds_left % 3600;
+            
+        minutes = pad( parseInt(seconds_left / 60) );
+
+        seconds = pad( parseInt( seconds_left % 60 ) );
+
+        // print in html
+        $("#day").html(days) ;
+        $("#hour").html(hours) ;
+        $("#minute").html(minutes) ;
+        $("#second").html(seconds) ;
+        }
+
+        function pad(n) {
+            return (n < 10 ? '0' : '') + n;
+        }
+
+/////////////////////////////////////    form footer1    /////////////////////////////////////
+function footer1()
+        {
+            
+            var value_name = $(".nameFooter").val();
+            var value_mail = $(".emailFooter").val();
+            var value_phone= $(".phoneFooter").val();
+            
+            
+            if(value_name == "")
+            {
+                alert("you did not entered your name");
+                return false;
+            }    
+            else if(value_mail == "")
+            {
+                alert("you did not enter your email");
+                return false;
+            }
+             else if(value_phone == "")
+            {
+                alert("you have not entered your number phone");
+                return false;
+            }
+            else{
+                confirm("Your information has been sent to us\nYour Name: "+value_name+"\nEmail: "+value_mail+"\nPhone: "+value_phone.replace(/...$/, 'xxx'))
+            }
+    }
+
+//////////////////////////////////    Feedback    /////////////////////////////////////////////////////
+    function feedback(){
+        var  hobFeedback;
+        var hobFeedbackValue = document.getElementsByName("hobF");
+        for(var i in hobFeedbackValue )
+        {
+            if(hobFeedbackValue[i].checked === true  )
+            {
+                hobFeedback= document.getElementsByName("hobF")[i].value;
+            }
+        }
+        if(hobFeedback == null)
+        {
+            alert("You haven't chosen your gender yet");
+            return false;
+        }
+
+        var value_name = $(".name-feedback").val();
+        var value_mail = $(".mail-feedback").val();
+        var value_phone= $(".phone-feedback").val();
+        var value_comment=$(".comment").val();
+        
+        
+        if(value_name == "")
+        {
+            alert("you did not entered your name");
+            return false;
+        }    
+            else if(value_mail == "")
+        {
+            alert("you did not enter your email");
+            return false;
+        }
+            
+            else if(value_phone == "")
+        {
+            alert("you have not entered your number phone");
+            return false;
+        }
+        else if(value_comment == "")
+        {
+            alert("You have not entered a comment yet");
+            return false;
+        }
+        
+            confirm("Your information has been sent to us\nYour Name: "+value_name+"\nGender: "+hobFeedback+"\nEmail: "+value_mail+"\nPhone: "+value_phone.replace(/...$/, 'xxx')+"\n\nYour Comment: \n"+ value_comment)
+        
+    }
---
+++ js/product.js
@@ -0,0 +1,218 @@
+
+var shoppingCart = (function() {
+    // =============================
+    // Private methods and propeties
+    // =============================
+    cart = [];
+    
+    // Constructor các thuôc tính của món hàng
+    function Item(name, price, count) {
+      this.name = name;
+      this.price = price;
+      this.count = count;
+    }
+    
+    // Save cart setItem
+    function saveCart() {
+      sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
+    
+      // Load cart GetItem
+    function loadCart() {
+      cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+      // cart = JSON.parse(JSON.stringify(cart))
+    }
+    if (sessionStorage.getItem("shoppingCart") != null) {
+                          //  JSON.stringify(cart)!=null
+      loadCart();
+    }
+    
+  
+    // =============================
+    // Public methods and propeties
+    // =============================
+    // tạo ra 1 đố tượng obj
+    var obj = {};
+    
+    // Add to cart
+    // tạo ra 1 phương thức thêm hàng cho obj
+    obj.addItemToCart = function(name, price, count) { //trong phương thức này gồm 3 property là (tên, giá và số lượng)
+
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart[item].count ++;
+          saveCart();
+          return;
+        }
+      }
+      // nếu hàng chưa có trong giỏ thì sẻ tạo ra 1 đối tượng mới  và thêm vào giỏ với count ? do IT đặt
+      var item = new Item(name, price, count);
+      cart.push(item);
+      saveCart();
+    }
+
+    // Set count from item
+    obj.setCountForItem = function(name, count) {
+      for(var i in cart) {
+        if (cart[i].name === name) {
+            cart[i].count = count;
+          break;
+        }
+      }
+    };
+
+    // Remove item from cart
+    obj.removeItemFromCart = function(name) {
+        for(var item in cart) {
+          if(cart[item].name === name) {
+            cart[item].count --;
+            if(cart[item].count === 0) {
+              cart.splice(item, 1); // món hàng tại vi5 trí  item sẽ bị xóa
+            }
+            break;
+          }
+      }
+      saveCart();
+    }
+  
+    // Remove all items from cart
+    obj.removeItemFromCartAll = function(name) {
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart.splice(item, 1);
+          break;
+        }
+      }
+      saveCart();
+    }
+  
+    // // Clear cart
+    // obj.clearCart = function() {
+    //   cart = [];
+    //   saveCart();
+    // }
+  
+    // Count cart tổng số lượng hàng hóa 
+    obj.totalCount = function() {
+      var totalCount = 0;
+      for(var item in cart) {
+        totalCount += cart[item].count;
+      }
+      return totalCount;
+    }
+  
+    // Total cart tổng tiền
+    obj.totalCart = function() {
+      var totalCart = 0;
+      for(var item in cart) {
+        totalCart += cart[item].price * cart[item].count;
+      }
+      return Number(totalCart.toFixed(2));
+    }
+  
+    // List cart  (copy lại)
+    obj.listCart = function() {
+      var cartCopy = [];
+      for(i in cart) {
+        item = cart[i];
+        itemCopy = {};
+        for(p in item) {
+          itemCopy[p] = item[p];
+  
+        }
+        itemCopy.total = Number(item.price * item.count).toFixed(2);
+        cartCopy.push(itemCopy)
+      }
+      return cartCopy;
+    }
+  
+    // cart                 : Array
+    // Item                 : Object/Class
+    // addItemToCart        : Function
+    // removeItemFromCart   : Function
+    // removeItemFromCartAll : Function
+    // clearCart            : Function
+    // countCart            : Function
+    // totalCart            : Function
+    // listCart             : Function
+    // saveCart             : Function
+    // loadCart             : Function
+    return obj;
+  })();
+  
+  
+  // *****************************************
+  // Triggers / Events
+  // ***************************************** 
+  // Add item
+  $('.add-to-cart').click(function(event) {
+    event.preventDefault(); // preventDefault dùng để chặn chuyển trang
+    var name = $(this).data('name');
+    var price = Number($(this).data('price'));
+    shoppingCart.addItemToCart(name, price, 1);
+    displayCart();
+  });
+  
+  // Clear items
+  $('.clear-cart').click(function() {
+    shoppingCart.clearCart();
+    displayCart();
+  });
+  
+  
+  function displayCart() {
+    var cartArray = shoppingCart.listCart();
+    var output = " ";
+    for(var i in cartArray) {
+      output += "<tr>"
+      + "<td>" + cartArray[i].name + "</td>" 
+      + "<td>(" + cartArray[i].price + "$ )</td>"
+      + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+      + "<input type='number' class='item-count amount' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+      + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+      + " = " 
+      + "<td>" + cartArray[i].total + "$ </td>" 
+      + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+      +  "</tr>";
+    }
+    $('.show-cart').html(output);
+    $('.total-cart').html(shoppingCart.totalCart());
+    $('.total-count').html(shoppingCart.totalCount());
+  }
+
+
+  // tới các chức năng
+  
+  // Delete item button
+  
+  $('.show-cart').on("click", ".delete-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.removeItemFromCartAll(name);
+    displayCart();
+  })
+  
+  
+  // -1
+  $('.show-cart').on("click", ".minus-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.removeItemFromCart(name);
+    displayCart();
+  })
+  // +1
+  $('.show-cart').on("click", ".plus-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.addItemToCart(name);
+    displayCart();
+  })
+  
+  // Item count input
+  $('.show-cart').on("change", ".item-count", function(event) {
+     var name = $(this).data('name');
+     var count = Number($(this).val());
+    shoppingCart.setCountForItem(name, count);
+    displayCart();
+
+  });
+  
+  displayCart();
+  
