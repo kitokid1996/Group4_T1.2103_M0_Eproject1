dd16078d36bd49ffe24fa25e6af2de96ad1f12a6
duy039 <hoperayno039@gmail.com>
2021-07-08 12:09:45 +0700

    responsive Nav Footer ok

--- js/main.js
+++ js/main.js
@@ -153,4 +153,13 @@ function footer1()
         }
         $("#printComment").append("<p> <span style=' color: red;'>Your: </span><span>"+ val_comment +"</span></p>");
     }
+//////////////////////////////////    nav    /////////////////////////////////////////////////////
     
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
